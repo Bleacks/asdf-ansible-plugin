3e088052672743da8804c1aa7e5d78c505065925
Bleacks <>
2021-01-29 01:46:32 +0100

    Patched list-all versions

--- lib/utils.bash
+++ lib/utils.bash
@@ -26,7 +26,7 @@ sort_versions() {
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' | grep -Ev '^[0-9].*' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    sed 's/^v//' | grep -E '^[0-9].*' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
 list_all_versions() {
